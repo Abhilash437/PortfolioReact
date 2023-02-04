a785490d2991ff4f9d38a184e1e0fa52faef6f0b
Abhilash437 <abhilashhathwar20@gmail.com>
2023-02-04 22:28:52 +0530

    build done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import {IoMenu} from 'react-icons/io5';
 import { Outlet } from 'react-router-dom';
 import { motion } from 'framer-motion';
+import logo from '../assets/Images/A.png';
 
 
 const Navbar = () => {
@@ -11,13 +12,15 @@ const Navbar = () => {
       <>
         <nav className="w-full px-6 z-50 fixed inset-x-0 top-2 flex justify-center items-center">
             <div className='w-full md:w-880 bg-navBar p-4 rounded-2xl flex items-center'>
-            <p className='text-lg text-slate-200 font-medium'>Abhilash</p>
+            <p className='text-lg text-slate-200 font-medium'>
+                <img src={logo} className='w-12'></img>
+            </p>
             <div className='hidden md:flex items-center gap-6 ml-6 flex-1'>
                 <a href="/Home" className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>Home</a>
                 <a href="/About" className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>About</a>
                 <a href="/Projects" className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>Projects</a>
                 <a href="/Contact" className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>Contact</a>
-                <motion.a whileTap={{scale:0.8}} href="/Home" className='ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out'>Download</motion.a>
+                <motion.a whileTap={{scale:0.8}} href="https://drive.google.com/file/d/19UsC6WLmm2BMr4QBW-AR0WHGu2diUPHd/view?usp=sharing" className='ml-auto text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out'>Download</motion.a>
             </div>
             <motion.div whileTap={{scale:0.5}} className='block md:hidden ml-auto cursor-pointer' onClick={(e)=>setIsActive(!isActive)}>
                 <IoMenu className='text-2xl text-textBase'></IoMenu>
@@ -29,7 +32,7 @@ const Navbar = () => {
                     <a href="/About" onClick = {()=>setIsActive(false)}className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>About</a>
                     <a href="/Projects" onClick = {()=>setIsActive(false)}className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>Projects</a>
                     <a href="/Contact" onClick = {()=>setIsActive(false)}className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'>Contact</a>
-                    <a href="/Home" onClick = {()=>setIsActive(false)}className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out'>Download</a>
+                    <a href="https://drive.google.com/file/d/19UsC6WLmm2BMr4QBW-AR0WHGu2diUPHd/view?usp=sharing" onClick = {()=>setIsActive(false)}className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer border border-textBase px-2 py-1 rounded-xl hover:border-gray-100 duration-100 ease-in-out'>Download</a>
                 </motion.div>
                 )
             }
