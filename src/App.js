0532ca92ceccad03f3f30e177f01842d193ab707
Abhilash437 <abhilashhathwar20@gmail.com>
2023-02-04 22:49:49 +0530

    final

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <BrowserRouter>
         <Routes>
           <Route path="/" element={<Navbar/>}>
-            <Route index path="/Home" element={<Home/>}/>
+            <Route exact path="/" element={<Home/>}/>
             <Route path="/About" element={<About/>}/>
             <Route path="/Projects" element={<Projects/>}/>
             <Route path="/Contact" element={<Contact/>}/>  
